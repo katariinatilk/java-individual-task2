0650e43d7978f6c4059f1ee4b565f426ee1a5ad8
katariinatilk <165931647+katariinatilk@users.noreply.github.com>
2024-06-10 22:08:33 +0300

    Update BookLibrary.java

--- BookLibrary.java
+++ BookLibrary.java
@@ -1,1 +1,22 @@
+import java.util.ArrayList;
 
+public class BookLibrary {
+
+private ArrayList<Book> books = new ArrayList<Book>();
+
+public void addBook(Book book){
+  books.add(book);
+}
+
+public void removeBook(Book book){
+  books.remove(book);
+}
+
+public ArrayList<Book> getBooks(){
+  return books; 
+  }
+}
+
+
+// sorting?
+// checking dups
